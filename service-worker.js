36720fcc9483b57e6e75016c9766b7d456321baa
zaksotm <zak@sotm.com>
2020-03-10 13:21:33 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/design-tool/precache-manifest.506ec0303d6d467c962905aa1e66e80d.js"
+  "/design-tool/precache-manifest.ce771a1dbdffcc5e1ddd4f9508ba7f2f.js"
 );
 
 self.addEventListener('message', (event) => {
